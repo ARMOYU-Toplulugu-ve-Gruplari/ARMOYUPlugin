4f56e9b2582788cd45feafb50c47d4feca893934
berkaytikenoglu <berkaytikenoglu@gmail.com>
2022-08-19 14:47:50 +0300

    Initial commit

---
+++ src/main/java/armoyuplugin/armoyuplugin/ARMOYUPlugin.java
@@ -0,0 +1,17 @@
+package armoyuplugin.armoyuplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ARMOYUPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
